fdf652064841c420cf7bd61f16481a4666f092d2
Kartikey Parashar <60282740+KartikeyParashar@users.noreply.github.com>
2020-01-30 19:12:27 +0530

    Update 2DArray.py

--- 2DArray.py
+++ 2DArray.py
@@ -1,3 +1,11 @@
+# 2D Array
+# a. Desc ­> A library for reading in 2D arrays of integers, doubles, or booleans from
+# standard input and printing them out to standard output.
+# b. I/P ­> M rows, N Cols, and M * N inputs for 2D Array. Use Java Scanner Class
+# c. Logic ­> create 2 dimensional array in memory to read in M rows and N cols
+# d. O/P ­> Print function to print 2 Dimensional Array. In Java use PrintWriter with
+# OutputStreamWriter to print the output to the screen.
+
 rows = int(input("Enter number of Rows: "))
 columns = int(input("Enter number of columns: "))
 
