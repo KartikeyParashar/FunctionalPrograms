3303b2fb221b5defaa42c75f5f90f24ffd064483
Kartikey Parashar <60282740+KartikeyParashar@users.noreply.github.com>
2020-01-30 19:13:44 +0530

    Update CouponNumbers.py

--- CouponNumbers.py
+++ CouponNumbers.py
@@ -1,3 +1,12 @@
+# a. Desc ­> Given N distinct Coupon Numbers, how many random numbers do you
+# need to generate distinct coupon number? This program simulates this random
+# process.
+# b. I/P ­> N Distinct Coupon Number
+# c. Logic ­> repeatedly choose a random number and check whether it's a new one.
+# d. O/P ­> total random number needed to have all distinct numbers.
+# e. Functions => Write Class Static Functions to generate random number and to
+# process distinct coupons.
+
 from random import randint
 coupons_want = int(input("How many distinct coupon number you want to generate: "))
 i=0
