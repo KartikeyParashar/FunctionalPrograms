6b5882f4f3786dfecde9ef4cfd37501272067334
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-01-31 16:33:43 +0530

    PrimeNumberBetweenAgivenRange Program

---
+++ check_primeNumbers_between_0and1000.py
@@ -0,0 +1,19 @@
+# Take a range of 0 ­ 1000 Numbers and find the Prime numbers in that range.
+
+import math
+
+# Store all programs in empty list
+prime_numbers = []
+
+
+for i in range(2,1000):
+    flag=0
+    for j in range(2,int(math.sqrt(i+1))+1):
+        if i%j==0:
+            flag=1
+            break
+    if flag==0:
+        prime_numbers.append(i)
+
+print(prime_numbers)
+
